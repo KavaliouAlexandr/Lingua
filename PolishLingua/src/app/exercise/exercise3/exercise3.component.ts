004c9d4684653476bcd4ae01718eae2ae4a71086
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-05-13 19:37:02 +0200

    excersize3

--- PolishLingua/src/app/exercise/exercise3/exercise3.component.ts
+++ PolishLingua/src/app/exercise/exercise3/exercise3.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercise3',
   templateUrl: './exercise3.component.html',
   styleUrls: ['./exercise3.component.scss']
 })
-export class Exercise3Component {
+export class Exercise3Component implements OnInit {
+  isAnswerCorrect: boolean | null = null;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectAnswer(isTrue: boolean) {
+    // Здесь можно добавить логику для проверки ответа
+    this.isAnswerCorrect = isTrue;
+  }
 }
